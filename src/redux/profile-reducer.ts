71ba7a417db3434ed1673109c7afa75d62111f62
Valeriy Litvinov <valitvinoff@mail.ru>
2022-11-02 23:14:38 +0300

    48 lesson - stateCopy into DialogReducer

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -34,16 +34,10 @@ export const profileReducer = (state: ProfilePageType = initialState, action: Ac
                     post: action.postText,
                     likesCount: 0
                 }
-            let stateCopy = {...state}
-            stateCopy.posts = [...state.posts]
-            stateCopy.posts.push(newPost)
-            stateCopy.newPostText = ''
-            return stateCopy
+            return {...state, posts: [...state.posts, newPost], newPostText: ''}
         }
-        case UPDATE_NEW_POST_TEXT:{
-            let stateCopy = {...state}
-            stateCopy.newPostText = action.newText
-            return stateCopy
+        case UPDATE_NEW_POST_TEXT: {
+            return {...state, newPostText: action.newText}
         }
         default:
             return state
