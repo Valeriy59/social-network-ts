87a352f5acb1eb5c6cf067740e2f7fc7b005d877
Valeriy Litvinov <valitvinoff@mail.ru>
2022-12-19 22:09:23 +0300

    84 lesson - useState ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import {ProfileUserType} from "../../../redux/profile-reducer";
-import userPhoto from "../../../assets/images/img.jpg";
-import ProfileStatus from "./ProfileStatus"
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 type ProfileInfoType ={
     profile: ProfileUserType,
@@ -20,7 +19,7 @@ const ProfileInfo = (props: ProfileInfoType) => {
             <div className={s.descriptionBlock}>
                 {/*<img src={props.profile.photos.large !== null ? props.profile.photos.large : userPhoto}/>*/}
                 {/*<span>{props.profile.aboutMe}</span>*/}
-                <ProfileStatus
+                <ProfileStatusWithHooks
                     status={props.status}
                     updateUserStatus={props.updateStatus}
                 />
---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,49 @@
+import React, {ChangeEvent, useState} from "react";
+
+
+type ProfileStatusPropsType = {
+    status: string
+    updateUserStatus: (status: string) => void
+}
+type ProfileStatusStateType = {
+    status: string
+    editMode: boolean
+}
+
+const ProfileStatusWithHooks = (props: ProfileStatusPropsType) => {
+    let [editMode, setEditMode] = useState(false)
+    let [status, setStatus] = useState(props.status)
+    const activateEditMode = () => {
+        setEditMode(true)
+    }
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateUserStatus(status)
+    }
+    const onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value)
+    }
+    return (
+        <div>
+            {!editMode &&
+                <div>
+                    <span onDoubleClick={activateEditMode}>{props.status || "----"}</span>
+                </div>
+            }
+            {editMode &&
+                <div>
+                    <input
+                        autoFocus={true}
+                        onBlur={deactivateEditMode}
+                        value={status}
+                        onChange={onChangeStatus}
+                    />
+                </div>
+            }
+        </div>
+
+    )
+
+}
+
+export default ProfileStatusWithHooks
